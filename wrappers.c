4c7c4f887d35e51f9ef4cedbe0132d8c98fc93ec
Joerg Walter <jwdev@posteo.de>
2021-02-11 15:41:51 +0100

    -new wrapper for strdup. -common malloc error function for wrappers.

--- wrappers.c
+++ wrappers.c
@@ -1,25 +1,36 @@
 /* This file is part of convertmdinfo, (c) 2021 Joerg Walter */
 #include <stdio.h>
+#include <string.h>
 #include "wrappers.h"
 
 #define ERROR_OOM "FATAL: Memory allocation error.\n"
 
+/* memfail prints an error message that no memory could be allocated and exits. */
+static void memfail() {
+	fprintf(stderr, ERROR_OOM);
+	exit(EXIT_FAILURE);
+}
+
 /* wrapper for malloc that exits the program on error */
 void* md_malloc(size_t size) {
 	void* ptr = malloc(size);
-	if (ptr == NULL) {
-		fprintf(stderr, ERROR_OOM);
-		exit(EXIT_FAILURE);
-	}
+	if (ptr == NULL)
+		memfail();
 	return ptr;
 }
 
 /* wrapper for realloc that exits the program on error */
 void* md_realloc(void *ptr, size_t size) {
 	void* ret_ptr = realloc(ptr, size);
-	if (ptr == NULL) {
-		fprintf(stderr, ERROR_OOM);
-		exit(EXIT_FAILURE);
-	}
+	if (ret_ptr == NULL)
+		memfail();
 	return ret_ptr;
+}
+
+char* md_strdup (const char* s) {
+	char* ptr = strdup(s);
+	if (ptr == NULL) {
+		memfail();
+	}
+	return ptr;
 }
--- wrappers.h
+++ wrappers.h
@@ -3,3 +3,4 @@
 
 void* md_malloc(size_t size);
 void* md_realloc(void *ptr, size_t size);
+char* md_strdup (const char* s);
