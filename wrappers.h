1ebb5cedc78a7c1e0e696b9156641145e5931552
Joerg Walter <jwdev@posteo.de>
2021-02-11 16:54:15 +0100

    -fixed missing include guard in wrappers.h

--- wrappers.h
+++ wrappers.h
@@ -1,6 +1,11 @@
 /* This file is part of convertmdinfo, (c) 2021 Joerg Walter */
+#ifndef _INCL_MDINFO
+#define _INCL_MDINFO
+
 #include <stdlib.h>
 
 void* md_malloc(size_t size);
 void* md_realloc(void *ptr, size_t size);
 char* md_strdup (const char* s);
+
+#endif
