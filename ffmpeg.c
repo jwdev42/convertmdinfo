86bec8994e0377bc684f2320bb154a82f91bb67a
Joerg Walter <jwdev@posteo.de>
2021-03-11 16:15:03 +0100

    -new more flexible way to scrape side channel data with ffmpeg, not active yet.

--- ffmpeg.c
+++ ffmpeg.c
@@ -215,3 +215,160 @@ int ffmpeg_recv_meta(const char *path, disp_meta *meta, disp_lum *lum) {
     ffbucket_free(bucket);
     return 0;
 }
+
+int ffmpeg_access_sidedata(const char *path, FILE *ostream,
+                           ff_return_t (*recv_func)(FILE *,
+                                                    AVFrameSideData *)) {
+    /* initialize */
+    ffbucket *bucket = ffbucket_alloc();
+
+    /* enable for debugging */
+    // av_log_set_level(AV_LOG_DEBUG);
+
+    /* open file */
+    if (avformat_open_input(&bucket->fmt_ctx, path, NULL, NULL) != 0)
+        return fferror(bucket, "ffmpeg could not open file");
+
+    if (avformat_find_stream_info(bucket->fmt_ctx, NULL) < 0)
+        return fferror(bucket, "ffmpeg could not retreive stream info");
+
+    /* find video stream */
+    int video_id = av_find_best_stream(bucket->fmt_ctx, AVMEDIA_TYPE_VIDEO, 0,
+                                       -1, NULL, 0);
+    if (video_id < 0)
+        return fferror(bucket, "No video stream in input file");
+
+    /* verify hevc codec */
+    AVCodecParameters *codec_par = bucket->fmt_ctx->streams[video_id]->codecpar;
+    if (codec_par->codec_id != AV_CODEC_ID_HEVC)
+        return fferror(bucket,
+                       "Video stream in input file is not an HEVC stream");
+
+    bucket->decoder = avcodec_find_decoder(codec_par->codec_id);
+    if (bucket->decoder == NULL)
+        return fferror(bucket, "Could not open ffmpeg HEVC decoder");
+
+    bucket->dec_ctx = avcodec_alloc_context3(bucket->decoder);
+    /* copy stream header information to codec context */
+    if (avcodec_parameters_to_context(bucket->dec_ctx, codec_par) < 0)
+        return fferror(bucket,
+                       "Could not copy codec parameters to codec context");
+
+    /* open AVCodecContext */
+    if (avcodec_open2(bucket->dec_ctx, bucket->decoder, NULL) < 0)
+        return fferror(bucket, "Could not initialize ffmpeg AVCodecContext");
+
+    bucket->pkt = av_packet_alloc();
+    av_init_packet(bucket->pkt);
+    bucket->pkt->data = NULL;
+    bucket->pkt->size = 0;
+    bucket->pkt->stream_index = video_id;
+    bucket->frame = av_frame_alloc();
+    bool ffabort = false;
+    int fc = 0; /* frame counter */
+    while (true) {
+        if (av_read_frame(bucket->fmt_ctx, bucket->pkt) < 0 ||
+            fc >= MAX_FRAMES) {
+            break; /* end of stream or error*/
+        }
+        if (bucket->pkt->stream_index != video_id) {
+            av_packet_unref(bucket->pkt);
+            continue;
+        }
+        fc++;
+        /* send packet to decoder */
+        int send_status = avcodec_send_packet(bucket->dec_ctx, bucket->pkt);
+        if (send_status != 0) {
+            if (send_status == AVERROR(ENOMEM))
+                md_bug(__FILE__, __LINE__, true);
+            else if (send_status == AVERROR(EINVAL))
+                md_bug(__FILE__, __LINE__, true);
+            else if (send_status == AVERROR(EAGAIN))
+                md_bug(__FILE__, __LINE__, true);
+            else if (send_status == AVERROR_EOF)
+                md_bug(__FILE__, __LINE__, true);
+            else {
+                /* legitimate decoding error */
+                return fferror(bucket,
+                               "avcodec_send_packet returned decoding error");
+            }
+        }
+        while (true) {
+            int frame_status =
+                avcodec_receive_frame(bucket->dec_ctx, bucket->frame);
+            if (frame_status != 0) {
+                if (frame_status == AVERROR(EAGAIN)) {
+                    if (send_status == AVERROR(EAGAIN)) {
+                        /* this condition leads to undefined behaviour in ffmpeg
+                         * if not catched */
+                        return fferror(
+                            bucket,
+                            "avcodec_send_packet and avcodec_receive_frame "
+                            "both returned EAGAIN");
+                    }
+                    break;
+                } else if (frame_status == AVERROR_EOF)
+                    break;
+                else if (frame_status < 0) {
+                    return fferror(
+                        bucket,
+                        "avcodec_receive_frame returned decoding error");
+                    return -1;
+                } else
+                    md_bug(__FILE__, __LINE__, true);
+            }
+            for (int i = 0; i < bucket->frame->nb_side_data; i++) {
+                AVFrameSideData *sd = bucket->frame->side_data[i];
+                ff_return_t ret = recv_func(ostream, sd);
+                if (ret == FFRET_ERROR || ret == FFRET_DONE) {
+                    ffabort = true;
+                    break;
+                } else if (ret == FFRET_BREAK)
+                    break;
+            }
+            if (ffabort)
+                break;
+            av_frame_unref(bucket->frame);
+        }
+        if (ffabort)
+            break;
+        av_packet_unref(bucket->pkt);
+        av_init_packet(bucket->pkt);
+    }
+
+    if (!ffabort)
+        return fferror(bucket,
+                       "Video stream does not contain the desired side data");
+
+    ffbucket_free(bucket);
+    return 0;
+}
+
+ff_return_t ffmpeg_disp_meta(FILE *ostream, AVFrameSideData *sd) {
+    if (sd->type == AV_FRAME_DATA_MASTERING_DISPLAY_METADATA) {
+        /* these are the droids we are looking for */
+        AVMasteringDisplayMetadata *ffmeta =
+            (AVMasteringDisplayMetadata *)sd->data;
+        if (ffmeta->has_primaries && ffmeta->has_luminance) {
+            disp_meta *col = disp_meta_alloc();
+            disp_lum *lum = disp_lum_alloc();
+            conv_meta(col, ffmeta);
+            conv_lum(lum, ffmeta);
+            disp_meta_x265 *x265 = meta_to_x265(col, lum);
+            if (x265 == NULL) {
+                free(col);
+                free(lum);
+                return FFRET_ERROR;
+            }
+            fprintf(ostream, "%s\n", x265_str(x265));
+            free(x265);
+            free(col);
+            free(lum);
+            return FFRET_DONE;
+        } else {
+            md_error_custom("Incomplete mastering display metadata");
+            return FFRET_ERROR;
+        }
+    }
+    return FFRET_CONTINUE;
+}
--- ffmpeg.h
+++ ffmpeg.h
@@ -3,7 +3,21 @@
 #define _INCL_FFMPEG
 
 #include "mdinfo.h"
+#include <libavutil/frame.h>
+#include <stdio.h>
+
+typedef enum {
+    FFRET_ERROR,    /* error reported */
+    FFRET_CONTINUE, /* continue AVFrameSideData loop, more data from frame
+                       expected */
+    FFRET_BREAK,    /* break AVFrameSideData loop, done with this frame */
+    FFRET_DONE,     /* do not decode further frames, gleaned all information */
+} ff_return_t;
+
+int ffmpeg_access_sidedata(const char *path, FILE *ostream,
+                           ff_return_t (*recv_func)(FILE *, AVFrameSideData *));
 
 int ffmpeg_recv_meta(const char *path, disp_meta *meta, disp_lum *lum);
 
+ff_return_t ffmpeg_disp_meta(FILE *ostream, AVFrameSideData *sd);
 #endif
